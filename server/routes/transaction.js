c31b081155058501db80dcf19e201840500c02f8
Jackson Davis <jdaviskid27@yahoo.com>
2023-02-08 11:23:47 -0500

    please get uploaded

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -3,12 +3,36 @@ const transactionRoutes = express.Router();
 const transactionModel = require('../models/transactionModel');
 
 // Retrieves a list of all users and their followers.
-transactionRoutes.get("/transaction", async (req, res) => {
+transactionRoutes.get("/allTransactions", async (req, res) => {
     transactionModel.findAll().then(transaction =>{
         console.log(transaction)
         res.sendStatus(200)
     })
     .catch(err => console.log(err));
+});
+
+transactionRoutes.post("/createTransaction", async (req, res) => {
+    const { name, description, date, price, category, category2, recurring } = req.body;
+
+    //creates a new user
+    const createTransaction = new transactionModel({
+      name: name,
+      description: description,
+      date: date,
+      price: price,
+      category: category,
+      category2: category2,
+      recurring: recurring,
+    });
+
+    try {
+        const saveNewTransaction = await createTransaction.save();
+        return res.status(200).send(saveNewTransaction);
+      } catch (err) {
+        return res.status(400).send({ message: err.errors[0].message });
+      }
+
+
 });
 
 module.exports = transactionRoutes;
