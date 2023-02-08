3717a5ca72cbca0ed0e0f94fae4bba39379c58ae
Jack Davis <jdaviskid27@yahoo.com>
2023-02-08 12:10:09 -0500

    more changes to transaction route

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const transactionRoutes = express.Router();
 const transactionModel = require('../models/transactionModel');
+const userModel = require("../models/userModel");
 
 // Retrieves a list of all users and their followers.
 transactionRoutes.get("/allTransactions", async (req, res) => {
-    transactionModel.findAll().then(transaction =>{
-        console.log(transaction)
-        res.sendStatus(200)
+  await transactionModel
+    .findAll()
+    .then((transactions) => {
+      return res.status(200).send(transactions);
     })
-    .catch(err => console.log(err));
+    .catch((err) => res.status(409).send(err));
 });
 
+
+// Finish this
 transactionRoutes.post("/createTransaction", async (req, res) => {
-    const { name, description, date, price, category, category2, recurring } = req.body;
+    const { name, description, date, price, category, category2, recurring, id } = req.body;
 
     //creates a new user
     const createTransaction = new transactionModel({
@@ -25,13 +29,16 @@ transactionRoutes.post("/createTransaction", async (req, res) => {
       recurring: recurring,
     });
 
+  //   const user = await userModel.findOne({ where: { user_id: id } });
+  // // add project and user to the join table with the custom method:
+  // createTransaction.addUser(user);
+
     try {
         const saveNewTransaction = await createTransaction.save();
         return res.status(200).send(saveNewTransaction);
       } catch (err) {
         return res.status(400).send({ message: err.errors[0].message });
       }
-
 
 });
 
