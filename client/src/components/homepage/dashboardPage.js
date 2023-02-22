258f1f331c922f091aa34fb22a826b676f89af73
Jack Davis <jdaviskid27@yahoo.com>
2023-02-22 11:39:53 -0500

    did a rough draft of recommendation card

--- client/src/components/homepage/dashboardPage.js
+++ client/src/components/homepage/dashboardPage.js
@@ -4,9 +4,8 @@ import Container from 'react-bootstrap/Container';
 import {Col, Row} from 'react-bootstrap';
 import RecentTransactionCard from './recentTransactionCard';
 import MonthlyExpensesCard from './recurringExpensesCard';
+import RecommendationCard from './recommendationCard';
 import "../register/loginPage.css"
-
-const url = "http://localhost:8085/createTransaction";
 
 const DashboardPage = () => {
 
@@ -27,6 +26,14 @@ const DashboardPage = () => {
         </Col>
         <Col>
           <MonthlyExpensesCard />
+        </Col>
+      </Row>
+      <Row>
+      <Col>
+          <RecentTransactionCard />
+        </Col>
+        <Col>
+          <RecommendationCard />
         </Col>
       </Row>
     </Container>
---
+++ client/src/components/homepage/recommendationCard.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import getUserInfo from '../../utilities/decodeJwt';
+import {Button, Card} from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../register/loginPage.css"
+
+const url = `http://localhost:8085/recurringExpenses/${getUserInfo().user_id.toString()}`;
+
+const RecommendationCard = () => {
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: "0",
+      
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+      });
+
+const navigate = useNavigate();
+
+  const [user, setUser] = useState(null)
+  const [error, setError] = useState("")
+  const [number, setNumber] = useState(0)
+  const [yearlyNumber, setYearlyNumber] = useState(0)
+
+
+    useEffect(() => {
+
+        async function getNumber() {
+          axios
+            .get(url)
+            .then(({ data }) => {
+                var sum = 0;
+                data.forEach(transaction => {
+                    
+                    sum += parseFloat(transaction.price);
+                });
+                setNumber(sum.toFixed(2))
+                setYearlyNumber(number * 12)
+            })
+            .catch((err) => {});
+        }
+        
+        getNumber()
+        console.log(number);   
+        setUser(getUserInfo())
+    
+        
+        
+        return; 
+    }, [number.length]);  
+
+
+  return (
+          <Card className="text-center"style={{ background: "rgb(50,58,69)", marginTop: "15px"}}>
+            <Card.Header>
+              <h2 style={{ color: "#14A44D" }}>Recommendations</h2>
+            </Card.Header>
+            <Card.Body>
+                <h4 style={{ color: "white" }}>Max Car Price: {formatter.format(getUserInfo().income * .35)}</h4>
+                <h4 style={{ color: "white" }}>Max House Price WIP: {formatter.format(getUserInfo().income * 4)}</h4>
+            </Card.Body>
+            <Card.Footer>
+            </Card.Footer>
+          </Card>
+  );
+};
+
+export default RecommendationCard;
