06f02efa5211f40c00c3e60e99df422c8becf2f3
Jackson Davis <jdaviskid27@yahoo.com>
2023-04-25 22:25:10 -0400

    Can now click the ticker to go to yahoo finance

--- client/src/components/stockpage/listComp.js
+++ client/src/components/stockpage/listComp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 import "../register/loginPage.css"
 
 
@@ -13,11 +14,10 @@ const ListComp = (props) => {
     props.onDeleteClickHandler();
       
   }
-
   
   const ListItem = ({ticker, date, price, shares, percentage, currPrice, profit}) => (
-    <tr style={{color: "white"}}>
-      <td>{ticker}</td>
+    <tr style={{color: "white"}} >
+      <td><Link to={`https://finance.yahoo.com/quote/${ticker}`} style={{ textDecoration: 'none', color: 'white'}}>{ticker}</Link></td>
       {canShowPercent && <td style={{color: percentage < 0 ? "orangered" : "lightgreen"}}>{percentage}%</td>}
       {canShow && <td>{date}</td>}
       <td>${Number(price).toFixed(2)}</td>
