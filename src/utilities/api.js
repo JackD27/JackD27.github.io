0cd5a7a83903c6f589ee37199e3a8c91782c1891
Jackson Davis <jdaviskid27@yahoo.com>
2023-05-01 23:46:41 -0400

    api

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,5 +1,5 @@
 const key = "cdcnpbiad3ic4diedsrgcdcnpbiad3ic4diedss0";
-const link2 = "http://localhost:8085";
+const link2 = "https://moneypad-capstone.herokuapp.com";
 const link1 = "https://moneypad-capstone.herokuapp.com";
 
 export { key, link1, link2 };
