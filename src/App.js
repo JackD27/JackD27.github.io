5bfbf45b7dfdcfdb1ec9591276ad5d0896d171da
Jackson Davis <jdaviskid27@yahoo.com>
2024-02-10 01:35:02 -0500

    new website

--- src/App.js
+++ src/App.js
@@ -1,81 +1,25 @@
-import React from "react";
-import { Route, Routes, useLocation, BrowserRouter, Switch} from "react-router-dom";
-
-//Private Routes 
-import PrivateRoute from "./utilities/PrivateRoute";
-import Admin from "./components/misc/adminPage";
-
-// Navbar
-import Navbar from "./components/navbar";
-
-//Register Page
-import Login from "./components/register/loginPage";
-import Register from "./components/register/registerPage";
-
-//Utilities Page
-import Settings from "./components/misc/editUserPage";
-import ErrorPage from "./components/misc/errorPage";
-
-//Dashboard Page
-import AddTransaction from "./components/homepage/addTransactionComp";
-import RecurringExpenses from "./components/homepage/recurringExpensesPage";
-import Dashboard from "./components/homepage/dashboardPage";
-
-//Stock Page
-import InvestingPage from "./components/stockpage/investingPage";
-import WatchlistPage from "./components/stockpage/watchListPage"; 
-import StockPage from "./components/stockpage/stockPage";
-import AddStockItem from "./components/stockpage/addPortfolioComp";
-import AddWatchlistItem from "./components/stockpage/addWatchlistComp";
-
-// User Info
-import getUserInfo from "./utilities/decodeJwt";
-
-document.body.style = 'background: rgb(234,237,242)';
+import Header from './components/Header';
+import * as React from 'react';
+import Footer from './components/Footer';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Route, Routes } from "react-router-dom";
 
 
-const App = () => {
+import MainPage from './components/MainPage';
 
+function App() {
 
-  // function locationPlace(){
-  //   if(location.pathname === '/' || location.pathname === '/register')
-  //   {
-  //     return false
-  //   }
-  //   else{
-  //     return true
-  //   }
-
-  // }
-  
-  // if(!user && locationPlace()){
-  //    return(<ErrorPage></ErrorPage>) 
-  // }
-  
- 
-
-  return (
+   return (
     <>
-        {getUserInfo() ? <Navbar /> : null}
-        <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/admin" element={<PrivateRoute><Admin/></PrivateRoute>} />
-            <Route exact path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
-            <Route exact path="/addTransaction" element={<PrivateRoute><AddTransaction /></PrivateRoute>} />
-            <Route exact path="/addStock" element={<PrivateRoute><AddStockItem /></PrivateRoute>} />
-            <Route exact path="/addWatchlist" element={<PrivateRoute><AddWatchlistItem /></PrivateRoute>} />
-            <Route exact path="/stocks" element={<PrivateRoute><InvestingPage /></PrivateRoute>} />
-            <Route exact path="/allStocks" element={<PrivateRoute><StockPage /></PrivateRoute>} />
-            <Route exact path="/allWatchlist" element={<PrivateRoute><WatchlistPage /></PrivateRoute>} />
-            <Route exact path="/transactions" element={<PrivateRoute><RecurringExpenses /></PrivateRoute>} />
-            <Route exact path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
-            <Route exact path="*" element={<ErrorPage />} />
-        </Routes>
-        </>
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<MainPage />} />
+    </Routes>
+    <Footer />
+      </>
+
   );
-};
+}
 
-
-
-export default App
+export default App;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,33 @@
-import { GoMarkGithub } from "react-icons/go";
-import { GrLinkedin } from "react-icons/gr";
-import { BsTrello } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import { Link } from 'react-router-dom';
+import {iconArray }from '../constants';
 
-const Footer = () => {
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'© '}{new Date().getFullYear()}{'  '}
+        Jackson Davis
+    </Typography>
+  );
+}
+
+function Footer(props) {
 
   return (
-    <div style={{ marginTop: "1.5rem", marginBottom:'1.5rem', bottom: 0 }}>
-      <footer style={{ textAlign: "center" }}>
-      <Link to={`https://www.linkedin.com/in/jackson-davis-3696a9258/`} style={{ textDecoration: 'none', color: 'black'}}><GrLinkedin size={32} color="#0077B5"/> LinkedIn</Link>
-      <Link to={`https://github.com/JackD27/CapstoneProject`} style={{ textDecoration: 'none', color: 'black', marginLeft: '2rem'}}><GoMarkGithub size={32} color="#333"/> Github</Link>
-      <Link to={`https://trello.com/b/cE22K4xL/jacksons-capstone-project`} style={{ textDecoration: 'none', color: 'black', marginLeft: '2rem'}}><BsTrello size={32} color="#0079BF"/> Trello</Link>
-      </footer>
-    </div>
+    <Box component="footer" sx={{backgroundImage: 'linear-gradient(180deg, #FFF, #CEE5FD)', bgcolor:'background.paper', backgroundSize: '100% -10%', backgroundRepeat: 'no-repeat', width: '100%', py: 6, borderBottom: '1px solid', borderColor: 'divider'}}>
+      <Container maxWidth="lg" align='center' >
+      {iconArray.map((text, index) => (
+          <IconButton key={index} component={Link} to={text.link}>{text.icon}</IconButton>
+        ))}
+
+      <Copyright />
+      </Container>
+    </Box>
   );
-};
+}
 
 export default Footer;
