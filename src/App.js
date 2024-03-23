c8ef55361018e258e2c22c56c9f14d765b33f49e
Jackson Davis <jdaviskid27@yahoo.com>
2024-03-23 02:01:13 -0400

    might help

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import * as React from 'react';
 import Footer from './components/Footer';
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import ReactGA from 'react-ga4';
 import { Route, Routes } from "react-router-dom";
 import MainPage from './components/MainPage';
-
-const trackingId = "G-FDMQ8XNGRM";
-
-ReactGA.initialize(trackingId);
 
 function App() {
 
